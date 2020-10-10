550b5ce8c1af088b9dd8d5b5013f19adae313543
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-10 19:39:24 +0530

    Added : Employee Monthly Wage with Condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,29 +5,51 @@ import java.util.Random;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		int WAGE_PER_HR = 20;
 		int FULL_DAY_HRS = 8;
 		int PART_TIME_HRS = 4;
 		int NUM_OF_WORKING_DAYS = 20;
-		double daily_Wage = 0;
+		int MAX_HRS_IN_MONTH = 100;
+
+		int totalEmpHrs = 0;
+		int empHrs = 0;
+		int totalWorkingDays = 0;
 		double monthly_Wage = 0;
+
 		Random rand = new Random();
-		for (int i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+
+		while (totalEmpHrs != MAX_HRS_IN_MONTH && totalWorkingDays != NUM_OF_WORKING_DAYS) {
+
+			totalWorkingDays++;
+
 			int random = rand.nextInt(3);
+
 			switch (random) {
+
 			case 1:
-				daily_Wage = WAGE_PER_HR * FULL_DAY_HRS;
+				empHrs = FULL_DAY_HRS;
 				break;
+
 			case 2:
-				daily_Wage = WAGE_PER_HR * PART_TIME_HRS;
+				empHrs = PART_TIME_HRS;
 				break;
+
 			case 0:
+				empHrs = 0;
 				break;
 			}
-			monthly_Wage = monthly_Wage + daily_Wage;
+			
+			totalEmpHrs = totalEmpHrs + empHrs;
+		
 		}
+		
+		monthly_Wage = totalEmpHrs * WAGE_PER_HR;
+		
 		System.out.println("Monthly Wage : " + monthly_Wage);
+
 	}
+
 }
