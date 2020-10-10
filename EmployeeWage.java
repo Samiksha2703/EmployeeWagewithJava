2e8a47d994c6fcdffec6964326619e76f368889f
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-10 17:59:19 +0530

    Added : Code for Employee Daily Wage with addition of Part Time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,13 +9,17 @@ public class EmployeeWage {
 
 		int WAGE_PER_HR = 20;
 		int FULL_DAY_HRS = 8;
+		int PART_TIME_HRS = 4;
 		double daily_Wage;
 		Random rand = new Random();
-		int random = rand.nextInt(2);
+		int random = rand.nextInt(3);
 		if (random == 1) {
 			daily_Wage = WAGE_PER_HR * FULL_DAY_HRS;
-			System.out.println("Employee-Present" + " " + "daily_Wage =" + daily_Wage);
+			System.out.println("Employee - Present" + " " + "daily_Wage = " + daily_Wage);
+		} else if (random == 2) {
+			daily_Wage = WAGE_PER_HR * PART_TIME_HRS;
+			System.out.println("Employee - Present" + " " + "daily_Wage = " + daily_Wage);
 		} else
-			System.out.println("Employee-Absent" + " " + "Daily Wage = 0");
+			System.out.println("Employee - Absent" + " " + "Daily Wage = 0");
 	}
 }
