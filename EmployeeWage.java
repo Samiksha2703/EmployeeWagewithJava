4a1ada150c6e184f440090f042e91e1a5f6f1f37
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-10 17:44:34 +0530

    Added : Code for Employee Daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,11 +6,16 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		int WAGE_PER_HR = 20;
+		int FULL_DAY_HRS = 8;
+		double daily_Wage;
 		Random rand = new Random();
 		int random = rand.nextInt(2);
-		if (random == 1)
-			System.out.println("Employee-Absent");
-		else
-			System.out.println("Employee-Present");
+		if (random == 1) {
+			daily_Wage = WAGE_PER_HR * FULL_DAY_HRS;
+			System.out.println("Employee-Present" + " " + "daily_Wage =" + daily_Wage);
+		} else
+			System.out.println("Employee-Absent" + " " + "Daily Wage = 0");
 	}
 }
