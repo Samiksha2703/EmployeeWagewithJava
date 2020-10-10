6850e049394627e121d61257e74afed698437293
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-10 17:19:08 +0530

    Added : Code for Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,8 +1,16 @@
 package EmployeeWagePro;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random rand = new Random();
+		int random = rand.nextInt(2);
+		if (random == 1)
+			System.out.println("Employee-Absent");
+		else
+			System.out.println("Employee-Present");
 	}
 }
